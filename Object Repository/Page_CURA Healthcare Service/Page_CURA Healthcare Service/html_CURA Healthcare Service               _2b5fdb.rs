a9f1f200ffd55ed3ec19ff60f66547992bd8e3fc
Duc <paypal.dhtran789@gmail.com>
2020-10-13 18:36:23 +0700

    first commit

---
+++ Object Repository/Page_CURA Healthcare Service/Page_CURA Healthcare Service/html_CURA Healthcare Service               _2b5fdb.rs
@@ -0,0 +1,207 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>html_CURA Healthcare Service               _2b5fdb</name>
+   <tag></tag>
+   <elementGuidId>a9d80e90-aa2b-466f-9c8f-e65912732986</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*/text()[normalize-space(.)='']/parent::*</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>html</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>html</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>lang</name>
+      <type>Main</type>
+      <value>en</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+    
+    
+    
+
+    CURA Healthcare Service
+
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+
+    
+    
+    
+
+#katalon{font-family:monospace;font-size:13px;background-color:rgba(0,0,0,.7);position:fixed;top:0;left:0;right:0;display:block;z-index:999999999;line-height: normal} #katalon div{padding:0;margin:0;color:#fff;} #katalon kbd{display:inline-block;padding:3px 5px;font:13px Consolas,&quot;Liberation Mono&quot;,Menlo,Courier,monospace;line-height:10px;color:#555;vertical-align:middle;background-color:#fcfcfc;border:1px solid #ccc;border-bottom-color:#bbb;border-radius:3px;box-shadow:inset 0 -1px 0 #bbb;font-weight: bold} div#katalon-spy_elementInfoDiv {color: lightblue; padding: 0px 5px 5px} div#katalon-spy_instructionDiv {padding: 5px 5px 2.5px}
+
+
+
+
+
+    
+        
+        
+            CURA Healthcare
+        
+        
+            Home
+        
+                
+            Login
+        
+            
+
+
+
+
+    
+        CURA Healthcare Service
+        We Care About Your Health
+        
+        Make Appointment
+    
+
+
+
+    
+        
+            
+                Login
+                Please login to make appointment.
+                                    Login failed! Please ensure the username and password are valid.
+                            
+            
+                
+                    
+                        
+                            Demo account
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                        
+                            
+                                
+                                  
+                                  
+                                
+                            
+                        
+                    
+                    
+                        Username
+                        
+                            
+                        
+                    
+                    
+                        Password
+                        
+                            
+                        
+                    
+                    
+                        
+                            Login
+                        
+                    
+                
+            
+        
+    
+
+
+
+
+    
+        
+            
+                CURA Healthcare Service
+                
+                Atlanta 550 Pharr Road NE Suite 525Atlanta, GA 30305
+                
+                     (678) 813-1KMS
+                     info@katalon.com
+                    
+                
+                
+                
+                    
+                        
+                    
+                    
+                        
+                    
+                    
+                        
+                    
+                
+                
+                Copyright © CURA Healthcare Service 2020
+            
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+/html[1]</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//html</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/ts01.ts
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d2fc9eeb-e4e9-4c16-8af8-e832cc9c7f09</testSuiteGuid>
+   <testCaseLink>
+      <guid>3a28c496-6ebc-4ce9-96ac-82c155c95146</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>6588c76a-ea9d-4e41-b248-57cbd9cbff51</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/datafile</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>6588c76a-ea9d-4e41-b248-57cbd9cbff51</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>85949382-a2e5-4ed3-8178-14507fa1da3a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ffe527d-727a-402f-9981-25ecfe53ef09</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>e2984b8e-d21f-43c4-bed2-11d5fb165606</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/datafile</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>e2984b8e-d21f-43c4-bed2-11d5fb165606</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>a7c84a66-593a-49a0-88e1-cf2fd5cca8f0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e4f6eb7c-2e26-4e9d-8742-34d986096fb2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc03</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>9b66553c-5fb1-4ed4-86ab-520f39961ec9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/datafile</testDataId>
+      </testDataLink>
+   </testCaseLink>
+</TestSuiteEntity>
